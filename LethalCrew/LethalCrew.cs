35b06e425301b898f8b03e4c1aa37bdce0c1d38e
TeoQtllc <teoquitellic@hotmail.fr>
2024-01-22 12:04:38 +0100

    0.2
    First Major dev release
    - All Killable monsters patched
    - Config file implementation

--- LethalCrew/LethalCrew.cs
+++ LethalCrew/LethalCrew.cs
@@ -1,6 +1,7 @@
 ﻿using BepInEx;
 using HarmonyLib;
 using LethalCrew.Patches;
+using BepInEx.Configuration;
 
 namespace LethalCrew
 {
@@ -9,13 +10,41 @@ namespace LethalCrew
 	{
 		private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
 
+		// Config file binding
+		public static ConfigEntry<bool> configRewardCredits;
+		public static ConfigEntry<bool> configRewardQuota;
+
+		public static ConfigEntry<int> configCentipedeReward;
+		public static ConfigEntry<int> configSpiderReward;
+		public static ConfigEntry<int> configHoarderBugReward;
+		public static ConfigEntry<int> configFlowerManReward;
+		public static ConfigEntry<int> configCrawlerReward;
+		public static ConfigEntry<int> configNutcrackerReward;
+		public static ConfigEntry<int> configMaskedPlayerReward;
+		public static ConfigEntry<int> configDogReward;
+		public static ConfigEntry<int> configBaboonReward;
+
+
 		private void Awake()
 		{
-			// Plugin startup logic
 			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
 
-			harmony.PatchAll(typeof(LethalCrew));
-			harmony.PatchAll(typeof(KillEnemyPatch));
+			// Config initialization
+			configRewardCredits = Config.Bind("__General__", "DoRewardsGrantCredits", true, "Does killing an enemy grants credits ?");
+			configRewardQuota = Config.Bind("__General__", "DoRewardsGrantQuota", true, "Does killing an enemy grants quota ?");
+
+			configCentipedeReward = Config.Bind("__Rewards__", "CentipedeReward", 15, "How much does a Centipede/Snare flea grants upon death.");
+			configSpiderReward = Config.Bind("__Rewards__", "SpiderReward", 45, "How much does a Spider grants upon death.");
+			configHoarderBugReward = Config.Bind("__Rewards__", "HoarderBugReward", 25, "How much does a Hoarder bug grants upon death.");
+			configFlowerManReward = Config.Bind("__Rewards__", "FlowerManReward", 150, "How much does a Flower Man/Bracken grants upon death.");
+			configCrawlerReward = Config.Bind("__Rewards__", "CrawlerReward", 75, "How much does a Crawler/Thumper grants upon death.");
+			configNutcrackerReward = Config.Bind("__Rewards__", "NutcrackerReward", 250, "How much does a Nutcracker grants upon death.");
+			configMaskedPlayerReward = Config.Bind("__Rewards__", "MaskedPlayerReward", 35, "How much does a MaskedPlayer/Zombie grants upon death.");
+			configDogReward = Config.Bind("__Rewards__", "DogReward", 300, "How much does a Dog grants upon death.");
+			configBaboonReward = Config.Bind("__Rewards__", "BaboonHawkReward", 50, "How much does Baboon Hawk grants upon death.");
+
+			// Harmony patching
+			harmony.PatchAll(typeof(LethalCrewPatch));
 		}
 	}
 }
--- LethalCrew/Patches/KillEnemyPatch.cs
+++ LethalCrew/Patches/KillEnemyPatch.cs
@@ -2,18 +2,100 @@
 
 namespace LethalCrew.Patches
 {
-	[HarmonyPatch(typeof(EnemyAI))]
-	internal class KillEnemyPatch
+	[HarmonyPatch]
+	internal class LethalCrewPatch
 	{
-		[HarmonyPatch(typeof(EnemyAI), nameof(EnemyAI.KillEnemy))]
-		[HarmonyPostfix]
-		static void KillEnemyGrantQuotaPatch()
+		static void RewardEnemyKill(int _rewardValue, string _enemyName)
 		{
-			Terminal terminal = UnityEngine.Object.FindObjectOfType<Terminal>();
-			terminal.groupCredits += 100;
-			//TimeOfDay.Instance.quotaFulfilled += 100;
-			HUDManager.Instance.UIAudio.PlayOneShot(HUDManager.Instance.reachedQuotaSFX);
+			if (_rewardValue <= 0 || (LethalCrew.configRewardCredits.Value == false && LethalCrew.configRewardQuota.Value == false))
+				return;
 
+			if (LethalCrew.configRewardCredits.Value)
+			{
+				Terminal terminal = UnityEngine.Object.FindObjectOfType<Terminal>();
+				terminal.groupCredits += _rewardValue;
+			}
+
+			if (LethalCrew.configRewardQuota.Value)
+			{
+				TimeOfDay.Instance.quotaFulfilled += _rewardValue;
+				TimeOfDay.Instance.UpdateProfitQuotaCurrentTime();
+			}
+
+			HUDManager.Instance.UIAudio.PlayOneShot(HUDManager.Instance.reachedQuotaSFX);
+			HUDManager.Instance.DisplayTip("Congratulations !", "You killed " + _enemyName + " ! The Company granted you " + _rewardValue + " credits !");
+		}
+
+		// ___ Patching CENTIPEDE ___ //
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(CentipedeAI), nameof(CentipedeAI.KillEnemy))]
+		static void KillCentipedePatch()
+		{
+			RewardEnemyKill(LethalCrew.configCentipedeReward.Value, "a Snare flea");
+		}
+
+		// ___ Patching SPIDER ___ //
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(SandSpiderAI), nameof(SandSpiderAI.KillEnemy))]
+		static void KillSpiderPatch()
+		{
+			RewardEnemyKill(LethalCrew.configSpiderReward.Value, "a Spider");
+		}
+
+		// ___ Patching HOARDER BUG *YIPEEEEEE* ___ //
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(HoarderBugAI), nameof(HoarderBugAI.KillEnemy))]
+		static void KillHoarderBugPatch()
+		{
+			RewardEnemyKill(LethalCrew.configHoarderBugReward.Value, "a Hoarding Bug");
+		}
+
+		// ___ Patching FLOWER MAN ___ //
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(FlowermanAI), nameof(FlowermanAI.KillEnemy))]
+		static void KillFlowerManPatch()
+		{
+			RewardEnemyKill(LethalCrew.configFlowerManReward.Value, "a Bracken");
+		}
+
+		// ___ Patching CRAWLER  ___ //
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(CrawlerAI), nameof(CrawlerAI.KillEnemy))]
+		static void KillCrawlerPatch()
+		{
+			RewardEnemyKill(LethalCrew.configCrawlerReward.Value, "a Thumper");
+		}
+
+		// ___ Patching NUTCRACKER  ___ //
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(NutcrackerEnemyAI), nameof(NutcrackerEnemyAI.KillEnemy))]
+		static void KillNutcrackerPatch()
+		{
+			RewardEnemyKill(LethalCrew.configNutcrackerReward.Value, "a Nutcracker");
+		}
+
+		// ___ Patching MASKED PLAYER  ___ //
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(MaskedPlayerEnemy), nameof(MaskedPlayerEnemy.KillEnemy))]
+		static void KillMaskedPlayerPatch()
+		{
+			RewardEnemyKill(LethalCrew.configMaskedPlayerReward.Value, "a Nutcracker");
+		}
+
+		// ___ Patching DOG  ___ //
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(MouthDogAI), nameof(MouthDogAI.KillEnemy))]
+		static void KillDogPatch()
+		{
+			RewardEnemyKill(LethalCrew.configDogReward.Value, "an Eyeless Dog");
+		}
+
+		// ___ Patching BABOON BIRD ___ //
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(BaboonBirdAI), nameof(BaboonBirdAI.KillEnemy))]
+		static void KillBaboonBirdPatch()
+		{
+			RewardEnemyKill(LethalCrew.configBaboonReward.Value, "a Baboon hawk");
 		}
 	}
 }
